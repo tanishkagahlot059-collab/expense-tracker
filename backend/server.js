5ed5f3ac01d12245a34373a5495aaf996e47bbd4
tanishkagahlot059-collab <tanishkagahlot059@gmail.com>
2026-05-03 18:05:43 +0530

    message

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT ||4000;
 
 // MIDDLEWARE
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:4000",
+  origin: "http://localhost:4000",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));

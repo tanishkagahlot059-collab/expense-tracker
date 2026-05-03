ff0367fdebf0b74b2fce4ab8747bc10811b61609
tanishkagahlot059-collab <tanishkagahlot059@gmail.com>
2026-05-03 18:35:11 +0530

    message

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoute.js';
 import incomeRouter from './routes/incomeRoute.js';

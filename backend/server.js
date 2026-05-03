d4375a3edd9df71c5f137a9787e387239b58b032
tanishkagahlot059-collab <tanishkagahlot059@gmail.com>
2026-05-03 17:49:08 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT ||4000;
 
 // MIDDLEWARE
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
